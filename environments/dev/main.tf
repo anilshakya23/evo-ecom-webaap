3f0dd9984917219e75bd0751dfb71d5f6e39c95e
anilshakya23 <anilshakya23@gmail.com>
2025-09-11 12:22:09 +0530

    new

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,6 +4,7 @@ module "dev-rg" {
   resource_group_location = "East US"
 }
 
+
 module "dev-vnet" {
   depends_on               = [module.dev-rg]
   source                   = "../../modules/azurerm_vnet"
@@ -12,6 +13,7 @@ module "dev-vnet" {
   resource_group_name      = "dev-evo-ecom-webapp-rg"
   virtual_network_location = "East US"
 }
+
 
 module "dev-subnet" {
   depends_on            = [module.dev-vnet]
